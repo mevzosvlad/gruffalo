643bac5fd54612930930edb598ed1a31b0a52a1c
mevzosvlad <mevzos.vlad@gmail.com>
2019-05-27 21:44:58 +0200

    Improving file usage
    -- Added Djava.net.preferIPv4Stack=true to force java to listen IPv4
    -- Added comment about -h 
    -- Improved readability
    -- Added required args to run java command

--- gruffalo-standalone.sh
+++ gruffalo-standalone.sh
@@ -2,4 +2,13 @@
 
 mvn dependency:build-classpath -Dmdep.outputFile=classpath -q
 CLASSPATH=target/classes:`cat classpath`
-java -cp $CLASSPATH -Xmx2024m -Xms2024m com.outbrain.gruffalo.StandaloneGruffaloServer "$@"
+# run com.outbrain.gruffalo.StandaloneGruffaloServer -h to check available args and usage
+# change port and cluster before starting
+java \
+  -cp $CLASSPATH \
+  -Djava.net.preferIPv4Stack=true \
+  -Xmx2024m \
+  -Xms2024m \
+  com.outbrain.gruffalo.StandaloneGruffaloServer \
+  --port 2003 \
+  --cluster node01:2003,node02:2003,node03:2003
